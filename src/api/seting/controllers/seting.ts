c897e55116b2d66022ac030786a34090ce84459d
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-11-12 11:41:58 -0400

    ok

--- src/api/seting/controllers/seting.ts
+++
@@ -1,7 +0,0 @@
-/**
- * seting controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::seting.seting');
--- src/api/seting/routes/seting.ts
+++
@@ -1,7 +0,0 @@
-/**
- * seting router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::seting.seting');
